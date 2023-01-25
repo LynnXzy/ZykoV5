78de2a3dd8c5d8dd9526ee7e4a5014cbd994a5d0
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2023-01-25 11:37:39 +0800

    Update donasi1.js

--- plugins/donasi1.js
+++ plugins/donasi1.js
@@ -4,20 +4,15 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
 
 let str = `
-
-┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
-❏────
-
+_*DONASI BOTZ LYN*_
 ┌─「 Donasi • TopUp 」
-│ • *Digi:* [${global.pdigi}]
-│ • *Maxis:* [${global.pmaxis}]
-│ • *Ovo:* [${global.povo}]
+│ • *Digi:* [${global.pdana}]
+│ • *Maxis:* [${global.pgopay}]
 │ • *Link Aja:* [${global.plinkaja}]
 ❏────`
 let wibu = `https://telegra.ph/file/82f399cf9dd246029ad4a.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.send3ButtonDoc(m.chat, str, botdate,'Sewa Bot','.sewa','QR+PAYMENT','.donasi1','Menu','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.send3ButtonDoc(m.chat, str, botdate,'Sewa Bot','.sewa','donasi2','.donasi1','Menu','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "tautanwa",
     mediaType: "tautanwa",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
